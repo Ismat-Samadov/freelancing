85ed12eb7c0b57eccbd95528677ec4c36e7b4e97
Ismat-Samadov <ismetsemedov@gmail.com>
2023-07-10 16:00:15 +0400

    check

---
+++ auto/test.py
@@ -0,0 +1,13 @@
+import smtplib
+
+MY_EMAIL = "ismetsemedov@gmail.com"
+MY_PASSWORD = "lmjareknmmweotsp"
+TO_EMAIL= "Ismat.Samadov@kapitalbank.az"
+connection = smtplib.SMTP("smtp.gmail.com") # port=587
+connection.starttls()
+connection.login(MY_EMAIL, MY_PASSWORD)
+connection.sendmail(
+    from_addr=MY_EMAIL,
+    to_addrs=MY_EMAIL,
+    msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+)
