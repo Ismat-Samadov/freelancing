25d6a10dd4c81cbabb88c667a5cf0c06e1ede4d7
Ismat-Samadov <ismetsemedov@gmail.com>
2023-07-11 14:44:05 +0400

    check

--- auto/test.py
+++ auto/test.py
@@ -1,13 +1,15 @@
 import smtplib
-
-MY_EMAIL = "ismetsemedov@gmail.com"
-MY_PASSWORD = "lmjareknmmweotsp"
-TO_EMAIL= "Ismat.Samadov@kapitalbank.az"
-connection = smtplib.SMTP("smtp.gmail.com") # port=587
-connection.starttls()
-connection.login(MY_EMAIL, MY_PASSWORD)
-connection.sendmail(
-    from_addr=MY_EMAIL,
-    to_addrs=TO_EMAIL,
-    msg="Subject:Look Up\n\nThe ISS is above you in the sky."
-)
+import time
+while True:
+    time.sleep(2)
+    MY_EMAIL = "ismetsemedov@gmail.com"
+    MY_PASSWORD = "lmjareknmmweotsp"
+    TO_EMAIL= "Ismat.Samadov@kapitalbank.az"
+    connection = smtplib.SMTP("smtp.gmail.com") # port=587
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs=TO_EMAIL,
+        msg="Subject:Look Up\n\nThe ISS is above you in the sky."
+    )
